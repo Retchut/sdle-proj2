744eb0bb91ca2c776592748e9cb485d9bbd2006f
luismrguimaraes <guimaraesluis0@gmail.com>
2022-12-06 12:51:46 +0000

    more testing

--- src/proj2.js
+++ src/proj2.js
@@ -2,7 +2,7 @@ import GUN from "https://cdn.skypack.dev/gun";
 import { initializeCanvas, updateCanvas } from './canvas.js';
 import { printNode } from "./nodes.js";
 
-const gun = GUN({localStorage : false});
+var gun = GUN();
 
 //const node1Name = 'A';
 //const node1Canvas = initializeCanvas();
@@ -11,34 +11,58 @@ const gun = GUN({localStorage : false});
 
 //var node2Canvas = initializeCanvas(gun);
 
-const node2Name =  "Node 2";
+const node2Name =  "Node_2";
 var node2 = gun.get(node2Name)
-const canvas = initializeCanvas(gun);
+var canvas = initializeCanvas(gun);
 node2.get('canvas').put(initializeCanvas(gun))
 
 node2.get('iddd').put(node2Name)
 node2.get('subscriptions').put("aaaa")
 node2.get('subscriptions').put("ss")
 node2.get('subscriptions').put("ssss")
+//node2.get('canvas').once(data => console.log(data._['#']))  // test/node
 
 
-canvas.put(null)
+var gun2 = gun.get('data')
+gun2.put({
+  object1: {ii: "ii"},
+  object2: {oo: "oo"}
+})
+
+// set 'object3' to a new node
+gun2.get('object3').put({
+  field: 'value'
+})
+
+gun2.get('object3').put(null)
+
+console.log(gun2)
 
 var a = []
 canvas.map(function(item, key){ // print them back out
   a.push(item)
 });
-console.log(a)
+//console.log(a)
 
 
-console.log("node2:")
+//console.log("node2:")
 node2.map().once(function(item, key){
-    console.log("key: " + key)
-    console.log("value: " + item)
+    //console.log("key: " + key)
+    //console.log("value: " + item)
 });
 
 
 
+console.log("Friends test:")
+var alice = gun.get('alice').put({name: 'alice', age: 22});
+var bob = gun.get('bob').put({name: 'bob', age: 24});
+var bob2 = gun.get('bob').put(null);
+var carl = gun.get('carl').put({name: 'carl', age: 16});
+var dave = gun.get('dave').put({name: 'dave', age: 42});
+
+console.log(gun.get('bob'))
+
+console.log("END Friends test")
 
 
 /*
